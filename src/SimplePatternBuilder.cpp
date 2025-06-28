b6424b80b1d6f5d6d3ff396f0c1a772ff92b9447
Andreas Schmidt <andreas.schmidt@hof-university.de>
2025-06-28 14:29:54 +0200

    fix: fix out of bounds write

--- src/SimplePatternBuilder.cpp
+++ src/SimplePatternBuilder.cpp
@@ -34,7 +34,7 @@ void SimplePatternBuilder::generate_pattern(HammeringPattern &pattern, size_t mi
     next_pattern.frequency = 1;
     pattern.agg_access_patterns.push_back(next_pattern);
 
-    while(inner_length > 0) {
+    while(inner_length > 0 && current_length < target_length) {
       pattern.aggressors[current_length++] = aggressors[inner_length-- % num_aggs];
     }
   }
