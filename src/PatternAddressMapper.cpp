c39265a174081c5feee6e00959eeec1b1963e5b7
Andreas Schmidt <andreas.schmidt@hof-university.de>
2025-08-20 16:28:11 +0200

    fix: add default scheduling for skylake

--- src/PatternAddressMapper.cpp
+++ src/PatternAddressMapper.cpp
@@ -456,6 +456,8 @@ static SCHEDULING_POLICY get_default_scheduling_policy_for_uarch() {
     // @iamywang, Jul 17, 2024: add Comet Lake
     case Microarchitecture::INTEL_COMET_LAKE:
       return SCHEDULING_POLICY::FULL;
+    case Microarchitecture::INTEL_SKY_LAKE:
+      return SCHEDULING_POLICY::FULL;
     default:
       Logger::log_error(format_string("Error: No default scheduling policy available for microarchitecture %s.", to_string(uarch)));
       exit(EXIT_FAILURE);
